e0710d0e40883910311cbf003095deed202751e0
Ricardo Lamar Meneses <r.lamar@habanos.cu>
2022-12-08 16:52:33 -0500

    anade repositorio mock, y contexto de datos

--- src/CommandApi/Controllers/CommandController.cs
+++ src/CommandApi/Controllers/CommandController.cs
@@ -1,16 +1,36 @@
+using CommandApi.Data;
+using CommandApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace CommandApi.Controllers
+namespace CommandApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommandController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CommandController : ControllerBase
+    //Add the following code to our class
+    private readonly ICommandApiRepo _repository;
+    public CommandController(ICommandApiRepo repository)
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+        var commandItems = _repository.GetAllCommands();
+        return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+        var commandItem = _repository.GetCommandById(id);
+        if (commandItem == null)
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            return NotFound();
         }
+        return Ok(commandItem);
     }
 }
---
+++ src/CommandApi/Data/ICommandAPIRepo.cs
@@ -0,0 +1,13 @@
+using CommandApi.Models;
+
+namespace CommandApi.Data;
+
+public interface ICommandApiRepo
+{
+    bool SaveChanges();
+    IEnumerable<Command> GetAllCommands();
+    Command GetCommandById(int id);
+    void CreateCommand(Command cmd);
+    void UpdateCommand(Command cmd);
+    void DeleteCommand(Command cmd);
+}
