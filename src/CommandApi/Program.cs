ff9bb971ea490e104bd2fe33351c7ebc5cd624de
Ricardo Lamar Meneses <r.lamar@habanos.cu>
2022-12-09 13:24:17 -0500

    usando user secrets

--- src/CommandApi/Program.cs
+++ src/CommandApi/Program.cs
@@ -1,10 +1,21 @@
 using CommandApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ICommandApiRepo, SqlCommandApiRepo>();
+
+var pgsqlBuilder = new NpgsqlConnectionStringBuilder
+{
+    ConnectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection"),
+    Username = builder.Configuration["UserID"],
+    Password = builder.Configuration["Password"]
+};
+builder.Services.AddDbContext<CommandContext>
+(opt => opt.UseNpgsql(pgsqlBuilder.ConnectionString));
+
 builder.Services.AddDbContext<CommandContext>(
     opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
