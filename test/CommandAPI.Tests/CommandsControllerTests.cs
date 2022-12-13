ded5285756699f0f39aedd158c57269cd736fd7f
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-12-13 09:05:47 -0500

    test para create update pach y delete

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -148,6 +148,123 @@ public class CommandsControllerTests : IDisposable
         Assert.IsType<ActionResult<CommandReadDto>>(result);
     }
 
+    [Fact]
+    public void CreateCommand_ReturnsCorrectResourceType_WhenValidObjectSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(1)).Returns(new Command
+        {
+            Id = 1,
+            HowTo = "mock",
+            Platform = "Mock",
+            CommandLine = "Mock"
+        });
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.CreateCommand(new CommandCreateDto { });
+        //Assert
+        Assert.IsType<ActionResult<CommandReadDto>>(result);
+    }
+
+    [Fact]
+    public void CreateCommand_Returns201Created_WhenValidObjectSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(1)).Returns(new Command
+        {
+            Id = 1,
+            HowTo = "mock",
+            Platform = "Mock",
+            CommandLine = "Mock"
+        });
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.CreateCommand(new CommandCreateDto { });
+        //Assert
+        Assert.IsType<CreatedAtRouteResult>(result.Result);
+    }
+
+    [Fact]
+    public void UpdateCommand_Returns204NoContent_WhenValidObjectSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(1)).Returns(new Command
+        {
+            Id = 1,
+            HowTo = "mock",
+            Platform = "Mock",
+            CommandLine = "Mock"
+        });
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.UpdateCommand(1, new CommandUpdateDto { });
+        //Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public void UpdateCommand_Returns404NotFound_WhenNonExistentResourceIDSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(0)).Returns(() => null);
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.UpdateCommand(0, new CommandUpdateDto { });
+        //Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public void PartialCommandUpdate_Returns404NotFound_WhenNonExistentResourceIDSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(0)).Returns(() => null);
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.PartialCommandUpdate(0,
+        new Microsoft.AspNetCore.JsonPatch.JsonPatchDocument<CommandUpdateDto>
+        { });
+        //Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public void DeleteCommand_Returns204NoContent_WhenValidResourceIDSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(1)).Returns(new Command
+        {
+            Id = 1,
+            HowTo = "mock",
+            Platform = "Mock",
+            CommandLine = "Mock"
+        });
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.DeleteCommand(1);
+        //Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public void DeleteCommand_Returns_404NotFound_WhenNonExistentResourceIDSubmitted()
+    {
+        //Arrange
+        mockRepo.Setup(repo =>
+        repo.GetCommandById(0)).Returns(() => null);
+        var controller = new CommandController(mockRepo.Object, mapper);
+        //Act
+        var result = controller.DeleteCommand(0);
+        //Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
     private static List<Command> GetCommands(int num)
     {
         var commands = new List<Command>();
